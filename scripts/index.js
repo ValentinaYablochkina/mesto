212364aceefd54791d1252bdc15c269e91020d29
Valentina Yablochkina <tino4ka9215@mail.ru>
2022-01-30 22:28:25 +0300

    Исправлены ошибки после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
-let profileInfoOpenPopupButton = document.querySelector('.profile__edit-button-border')
+let profileInfoOpenPopupButton = document.querySelector('.profile__edit-button')
 let popup = document.querySelector('.popup')
 let popupCloseButton = document.querySelector('.popup__close')
 let popupSave = document.querySelector('.popup__save')
 let popupForm = document.querySelector('.popup__form')
 let profileName = document.querySelector('.profile__name')
 let profileProfession = document.querySelector('.profile__profession')
-let NameInput = document.getElementById('name')
-let ProfessionInput = document.getElementById('profession')
+let nameInput = document.getElementById('name')
+let professionInput = document.getElementById('profession')
 
 function OpenPopup() {
-  popup.classList.add('popup__opened')
-  popupCloseButton.classList.add('popup__opened')
+  popup.classList.add('popup_opened')
+  nameInput.value = profileName.textContent
+  professionInput.value = profileProfession.textContent
 }
 
 function ClosePopup() {
-  popup.classList.remove('popup__opened')
-  popupCloseButton.classList.remove('popup__opened')
+  popup.classList.remove('popup_opened')
 }
 
 function SubmitForm(evt) {
   evt.preventDefault()
-  profileName.textContent = NameInput.value
-  profileProfession.textContent = ProfessionInput.value
+  profileName.textContent = nameInput.value
+  profileProfession.textContent = professionInput.value
+  ClosePopup()
 }
 
 
 profileInfoOpenPopupButton.addEventListener('click', OpenPopup)
 popupCloseButton.addEventListener('click', ClosePopup)
 popupForm.addEventListener('submit', SubmitForm)
-popupSave.addEventListener('click', ClosePopup)
